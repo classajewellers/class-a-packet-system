5549504c5f2b0262a05bc097b30e0bf2bea9e46f
Josh Mucklow <josh@classa.com.au>
2026-05-22 13:44:36 +0930

    Temporarily disable Basic Auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // Skip auth for API routes that need to be publicly accessible (Shopify webhook)
-  if (request.nextUrl.pathname.startsWith('/api/shopify')) {
-    return NextResponse.next()
-  }
-
-  const basicAuth = request.headers.get('authorization')
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
-
-    const validUser = process.env.BASIC_AUTH_USER
-    const validPassword = process.env.BASIC_AUTH_PASSWORD
-
-    if (user === validUser && pwd === validPassword) {
-      return NextResponse.next()
-    }
-  }
-
-  return new NextResponse('Authentication required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Class A Order System"',
-    },
-  })
+  return NextResponse.next()
 }
 
 export const config = {
